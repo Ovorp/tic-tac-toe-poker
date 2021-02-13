c90de2dcf8024013176c9fecae79fda76440adb4
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-13 14:46:58 -0700

    fixed a turn 9 bug

--- Part1/index.js
+++ Part1/index.js
@@ -21,25 +21,22 @@ function winner() {
             alert(`Winner is player ${keepingTrack[3]}`)
 
         } else if (keepingTrack[6] !== undefined && keepingTrack[6] === keepingTrack[7]  && keepingTrack[6] === keepingTrack[8]) {
-            alert(`Winner is player ${keepingTrack[0]}`)
+            alert(`Winner is player ${keepingTrack[6]}`)
 
         } else if (keepingTrack[0] !== undefined && keepingTrack[0] === keepingTrack[3]  && keepingTrack[0] === keepingTrack[6]) {
             alert(`Winner is player ${keepingTrack[0]}`)
 
         } else if (keepingTrack[1] !== undefined && keepingTrack[1] === keepingTrack[4]  && keepingTrack[1] === keepingTrack[7]) {
-            alert(`Winner is player ${keepingTrack[0]}`)
+            alert(`Winner is player ${keepingTrack[1]}`)
 
         } else if (keepingTrack[2] !== undefined && keepingTrack[2] === keepingTrack[5]  && keepingTrack[2] === keepingTrack[8]) {
-            alert(`Winner is player ${keepingTrack[0]}`)
+            alert(`Winner is player ${keepingTrack[2]}`)
 
         } else if (keepingTrack[0] !== undefined && keepingTrack[0] === keepingTrack[4]  && keepingTrack[0] === keepingTrack[8]) {
             alert(`Winner is player ${keepingTrack[0]}`)
 
         } else if (keepingTrack[2] !== undefined && keepingTrack[2] === keepingTrack[4]  && keepingTrack[2] === keepingTrack[6]) {
-            alert(`Winner is player ${keepingTrack[0]}`)
-        }
-    }
-    if (turn === 9) {
-        alert("CAT's Game")
+            alert(`Winner is player ${keepingTrack[2]}`)
+        } else if (turn === 9) {alert(`CAT's Game`)}
     }
 }
